f77b6b69023d713e7bde8c19a06e99e55d141229
Omar Lizardo <olizardo@soc.ucla.edu>
2024-05-15 13:30:21 -0700

    new tabs and updated salsa function

--- Functions/hits.R
+++ Functions/hits.R
@@ -1,24 +1,22 @@
-hits <- function(A, e = 0.000001) { #From Fouss et al. 2016, p. 226
+hits <- function(A, e = 0.000001) {
      n <- nrow(A)
      c.h <- 1
-     c.a <- 1
      a <- rep(1, n)/sqrt(n)  #initializing authority scores
      m.h <- a
      m.a <- a
      k <- 0 #initializing counter
-     while (c.h > e & c.a > e) {
+     while (c.h > e) {
           if (k > 0) {
                 o.h <- h
                 }
           o.a <- a
-          h <- A %*% a #hub scores a function of authority scores
+          h <- A %*% o.a #hub scores a function of previous authority scores
           h <- h/norm(h, type = "E")
-          a <- t(A) %*% h #authority scores a function of hub scores
+          a <- t(A) %*% h #authority scores a function of current hub scores
           a <- a/norm(a, type = "E")
           if (k > 0) {
-                c.h <- abs(sum(abs(o.h) - abs(h)))
+                c.h <- abs(sum(abs(o.h) - abs(h))) #diff. between old and new scores
                 }
-          c.a <- abs(sum(abs(o.a) - abs(a)))
           m.a <- cbind(m.a, a) #building authority score matrix
           m.h <- cbind(m.h, h) #building hub score matrix
           k <- k + 1 #incrementing counter
--- Functions/salsa.R
+++ Functions/salsa.R
@@ -1,4 +1,4 @@
-salsa <- function(A, iter = 50) { #From Fouss et al. 2016, p. 226
+salsa <- function(A, e = 0.000001) { #From Fouss et al. 2016, p. 226
      n <- nrow(A)
      c.h <- 1
      c.a <- 1
@@ -9,18 +9,24 @@ salsa <- function(A, iter = 50) { #From Fouss et al. 2016, p. 226
      Wr <- A/rowSums(A)
      Wc <- A/colSums(A)
      k <- 0 #initializing counter
-     while (k < iter) {
+     while (c.h > e) {
           o.h <- h
           o.a <- a
-          h <- Wc %*% o.a #hub scores a function of previous authority scores
-          h <- h/norm(h, type = "E")
-          a <- t(Wr) %*% o.h #authority scores a function of previous hub scores
+          a <- t(Wc) %*% o.h #authority scores a function of previous hub scores
           a <- a/norm(a, type = "E")
-          c.h <- abs(sum(abs(o.h) - abs(h)))
-          c.a <- abs(sum(abs(o.a) - abs(a)))
+          h <- Wr %*% o.a #hub scores a function of previous authority scores
+          h <- h/norm(h, type = "E")
           m.a <- cbind(m.a, a) #building authority score matrix
           m.h <- cbind(m.h, h) #building hub score matrix
+          if (k >= 5) {
+                  c.a <- abs(sum(abs(m.a[,k-2]) - abs(m.a[,k])))
+                  c.h <- abs(sum(abs(m.h[,k-2]) - abs(m.h[,k])))
+                }
           k <- k + 1 #incrementing counter
-     }
-     return(list(k = k, m.a = round(m.a, 4), m.h = round(m.h, 4)))
+        }
+     return(list(k = k, 
+                 m.a = round(m.a, 4), 
+                 m.h = round(m.h, 4),
+                 Wr = Wr,
+                 Wc = Wc))
 }
